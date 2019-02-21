3df8499a4c4b55dbf1336fdc53340f56660dd55d
SandeepGanesana <sandeep.ganesana@gmail.com>
2019-02-21 15:27:59 +0530

    nested forms Custom Validations

--- src/app/customers/customer.component.ts
+++ src/app/customers/customer.component.ts
@@ -1,13 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 // Custom Validation
+function ratingRange (min: number, max: number): ValidatorFn {
+  return (c: AbstractControl):{ [key: string]: boolean } | null => {
+    if (c.value !== null && (isNaN(c.value)|| c.value > max || c.value < min)) {
+      return { 'range': true };
+    }
+    return null;
+  };
+}
 
-function ratingRange (c: AbstractControl):{ [key: string]: boolean } | null { 
-  if (c.value !== null && (isNaN(c.value)|| c.value > 5 || c.value < 1)) {
-    return { 'range': true }
-  }
-  return null;
+// Email Match Validation
+function emailMatch(c: AbstractControl):{ [key: string]: boolean } | null {
+    const first = c.get('email');
+    const second = c.get('confirmEmail');
+    console.log("I'm from  emailMatch Validators", c)
+    if(first.pristine || second.pristine){
+      return null;
+    }
+
+    if (first.value === second.value){
+      return null;
+    }
+    return { 'match': true };
 }
 
 @Component({
@@ -17,6 +33,14 @@ function ratingRange (c: AbstractControl):{ [key: string]: boolean } | null {
 })
 export class CustomerComponent implements OnInit {
   customerForm: FormGroup;
+  emailMessage: string;
+  emailConfirmationMessage: string;
+
+  private validationMessage = {
+    required: 'Please Enter Your Email',
+    email: 'Please Enter Valid Email',
+    match: 'Confirm Your Email'
+  }
 
   constructor(private fb: FormBuilder) { }
 
@@ -24,13 +48,25 @@ export class CustomerComponent implements OnInit {
     this.customerForm = this.fb.group({
       firstName: ['', [Validators.required, Validators.minLength(4)]],
       lastName: ['', [Validators.required, Validators.maxLength(50)]],
-      email: ['', [Validators.email, Validators.required]],
+      emailGroup: this.fb.group({
+        email: ['', [Validators.email, Validators.required]],
+        confirmEmail: ['', [Validators.email, Validators.required]]
+      }, { validator: emailMatch }),
       sendCatalog: true,
       phone: [''],
       notification: 'email',
-      rating: [null, ratingRange]
+      rating: [null, ratingRange(1,5)]
     })
+    this.customerForm.get('notification').valueChanges.subscribe(
+      value => this.setNotification(value)
+    );
+
+    const emailControl = this.customerForm.get('emailGroup.email');
+    const emailConfirmControl = this.customerForm.get('emailGroup.confirmEmail');
+    emailControl.valueChanges.subscribe(value => this.setMessage(emailControl, 'email'));
+    emailConfirmControl.valueChanges.subscribe(value => this.setMessage(emailConfirmControl, 'confirmEmail'));
   }
+
 
   save() {
     console.log(this.customerForm);
@@ -46,12 +82,10 @@ export class CustomerComponent implements OnInit {
       phone: '',
       notification: 'email'
     })
-    this.notify('email');
+    this.setNotification('email');
   }
 
-
-  
-  notify(value: string){
+  setNotification(value: string){
     const phoneControl = this.customerForm.get('phone');
     if(value === 'mobile') {
       phoneControl.setValidators([Validators.minLength(10), Validators.required]);
@@ -60,4 +94,22 @@ export class CustomerComponent implements OnInit {
     }
     phoneControl.updateValueAndValidity();
   }
+
+  setMessage(c: AbstractControl, type: string):void {
+    this.emailMessage = '';
+    this.emailConfirmationMessage = '';
+    console.log(this.customerForm.get('emailGroup').errors)
+    if ((c.touched || c.dirty) && c.errors && type == 'email') {
+      this.emailMessage = Object.keys(c.errors).map(
+      key => {
+            return this.emailMessage += this.validationMessage[key];
+        }).join(' ');
+    } else if ((c.touched || c.dirty) && c.errors && type == 'confirmEmail') {
+      this.emailConfirmationMessage = Object.keys(c.errors).map(
+        key => {
+          return this.emailConfirmationMessage += this.validationMessage[key];
+        }).join(' ');
+    }
+  }
+
 }
